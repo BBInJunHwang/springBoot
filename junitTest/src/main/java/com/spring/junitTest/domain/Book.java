9aa50cecb27bf5e5714253d17d9a1bc0d089f13f
황인준 <rhdqor333@naver.com>
2022-10-24 16:37:22 +0900

    book entity 추가

--- junitTest/src/main/java/com/spring/junitTest/domain/Book.java
+++ junitTest/src/main/java/com/spring/junitTest/domain/Book.java
@@ -1,5 +1,36 @@
 package com.spring.junitTest.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity // 테이블 생성
+@Getter // setter 사용 금지
+@NoArgsConstructor
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 1씩 증가
+	private long id;
+	
+	@Column(length = 50, nullable = false)
+	private String title;
+	
+	@Column(length = 20, nullable = false)
+	private String author;
+	
+	@Builder
+	public Book(long id, String title, String author) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+	
+	
 }
--- junitTest/src/test/java/com/spring/junitTest/domain/BookTest.java
+++ junitTest/src/test/java/com/spring/junitTest/domain/BookTest.java
@@ -1,5 +1,25 @@
 package com.spring.junitTest.domain;
 
+import javax.persistence.Entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity // 테이블 생성
+@Getter // setter 사용 금지
+@NoArgsConstructor
 public class BookTest {
+	
+	private long id;
+	private String title;
+	private String author;
+	
+	@Builder	// 자유롭게 넣고 싶은 순서,값 넣을 수 있음
+	public BookTest(long id, String title, String author) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
 
 }
